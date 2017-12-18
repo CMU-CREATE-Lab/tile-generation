a8d9e7af33e750d778a9a21cc59694c7dd5ccc0a
Chris Bartley <bartley@cmu.edu>
2017-12-18 10:35:46 -0500

    Switched to klokantech-basic-cpb style, and bounds now default to entire planet

--- tile-fetcher/index.js
+++ tile-fetcher/index.js
@@ -1,20 +1,32 @@
 const TileFetcher = require('./TileFetcher');
 
+const STYLE_NAME = 'klokantech-basic-cpb';
+
 new TileFetcher(
       {
-         westLong : -125.3321, // west long
-         southLat : 23.8991,    // south lat
-         eastLong : -65.7421,   // east long
-         northLat : 49.4325, // north lat
+         // whole planet
+         westLong : -180,     // west long
+         southLat : -85.0511, // south lat
+         eastLong : 180,      // east long
+         northLat : 85.0511,  // north lat
+
+         // USA
+         // westLong : -125.3321, // west long
+         // southLat : 23.8991,    // south lat
+         // eastLong : -65.7421,   // east long
+         // northLat : 49.4325, // north lat
+
          zoomLevel : 10,
          numTileFetchers : 6,
+
          tileUrlBuilder : function(x, y, z) {
             // retina tiles
-            // return "http://localhost:8080/styles/osm-bright-cpb/" + z + "/" + x + "/" + y + "@2x.png";
+            // return "http://localhost:8080/styles/" + STYLE_NAME + "/" + z + "/" + x + "/" + y + "@2x.png";
 
             // non-retina tiles
-            return "http://localhost:8080/styles/osm-bright-cpb/" + z + "/" + x + "/" + y + ".png";
+            return "http://localhost:8080/styles/" + STYLE_NAME + "/" + z + "/" + x + "/" + y + ".png";
          },
-         tileDirectory : './tiles/osm-bright-cpb'
+
+         tileDirectory : './tiles/' + STYLE_NAME
       }
 ).run();
